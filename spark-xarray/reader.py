8db8475e62e2b8e638150cd90ea852d53d0f3b7a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-07-18 10:21:53 -0600

    Update requirements and travis.yml

--- spark-xarray/reader.py
+++ spark-xarray/reader.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import numpy as np
 import pandas as pd 
 import xarray as xr
-from pyspark.sql import SparkSession
+#from pyspark.sql import SparkSession
 
 a = xr.open_dataset('../sample-data/air.sig995.2012.nc')
 print(a)
