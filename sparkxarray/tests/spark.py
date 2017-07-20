a8af1251629485c74a1e05a16c0a2b7587304077
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-07-20 13:41:04 -0600

    Remove unnecessary instructions from .travis.yml

--- sparkxarray/tests/spark.py
+++ sparkxarray/tests/spark.py
@@ -1,15 +1,18 @@
 from __future__ import print_function
 import numpy as np 
 from pyspark.sql import SparkSession
+import xarray as xr
+
 
 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("test").getOrCreate()
     sc = spark.sparkContext
-    
-    a = np.arange(300).reshape(30, 10)
-    rdd = sc.parallelize(a, 10)
+
+    data = xr.DataArray(np.random.randn(2, 200), coords={'x': ['a', 'b']}, dims=('x', 'y'))
+    print(type(data))
+    rdd = sc.parallelize(data, 10)
     print(rdd.count())
-    print(rdd.collect())
+    print(rdd.take(2))
     
