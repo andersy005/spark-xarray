51dc81c415992cdd3653345b1420c1195d0af912
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-07-21 13:12:20 -0600

    Update installation configurations and instructions

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from distutils.core import setup
+"""Setup-module for spark-xarray.
+
+This software enables working with netCDF climate model data in Apache Spark.
+
+See: 
+https://github.com/andersy005/spark-xarray
+"""
+
+import os
+import re
+import sys
+import warnings
+from setuptools import setup
+from setuptools import find_packages
 
 setup(name='spark-xarray',
       maintainer='Anderson Banihirwe',
       maintainer_email='axbanihirwe@gmail.com',
       version='0.1.dev0',
-      description='PySpark for Big Atmospheric & Oceanic Data Analysis',
+      description='Big Atmospheric & Oceanic Data Analysis with Apache Spark with xarray',
       url='https://github.com/andersy005/spark-xarray',
-      packages=['sparkxarray']
+      install_requires=['xarray'],
+      packages=['sparkxarray'],
+      keywords=['xarray', 'Apache Spark', '']
       
 )
