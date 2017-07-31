3312743e9cd61d8dfd2bbfb4fb8be27823ff8dac
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-07-31 17:15:05 -0600

    Add multiple files support + test cases

--- sparkxarray/reader.py
+++ sparkxarray/reader.py
@@ -86,9 +86,9 @@ def read_nc_single(sc, paths, **kwargs):
     -----------
     sc       :  sparkContext object
 
-    paths    :  str or sequence
-                Either a string glob in the form "path/to/my/files/*.nc" or an explicit
-                list of files to open
+    paths    :  str
+                an explicit filename to open
+    
 
     **kwargs : dict
                Additional arguments for partitioning 
@@ -125,7 +125,7 @@ def read_nc_single(sc, paths, **kwargs):
 
 
 def readone_slice(dset, positional_indices):
-    """Read a slice from a xarray.Dataset.
+    """Read a slice from an xarray.Dataset.
 
     Parameters
     ----------
@@ -141,8 +141,8 @@ def readone_slice(dset, positional_indices):
     chunk               : xarray.Dataset
                          a subset of the Xarray Dataset
 
-
     """
+
     # Change the positional indices into slice objects
     # e.g {'lat': 0, 'lon': 0} ---> {'lat': slice(0, 1, None),  'lon': slice(0, 1, None)}
     positional_slices = {dim: slice(positional_indices[dim], positional_indices[dim]+1) 
@@ -152,6 +152,7 @@ def readone_slice(dset, positional_indices):
     chunk = dset[positional_slices]
     return chunk
 
+
 def read_nc_multi(sc, paths, **kwargs):
     """ Read multiple netCDF files
 
@@ -159,123 +160,41 @@ def read_nc_multi(sc, paths, **kwargs):
     -----------
     sc       :  sparkContext object
 
-    paths    :  str
-                an explicit filename to open
+    paths    :  str or sequence
+                Either a string glob in the form "path/to/my/files/*.nc" or an explicit
+                list of files to open
 
     **kwargs : dict
                Additional arguments for partitioning 
 
     """
+
+    partition_on = kwargs.get('partition_on')
+    partitions = kwargs.get('partitions')
+
     dset = xr.open_mfdataset(paths, autoclose=True)
 
+    # D = {'dim_1': dim_1_size, 'dim_2': dim_2_size, ...}
+    D ={dset[dimension].name:dset[dimension].size for dimension in partition_on}
+    
+    # dim_sizes = [range(dim_1_size), range(dim_2_size), range(...)]
+    dim_ranges = [range(dim_size) for dim_size in D.values()]
+    
 
+    dim_cartesian_product_indices = [element for element in itertools.product(*dim_ranges)]
 
-def nc_multi_read(sc, file_list, partitions=None, data_splitting_mode='slice'):
-    if (data_splitting_mode == 'slice'):
-        return read_nc_multi_time_slice(sc, file_list, partitions) 
-        
-    elif (data_splitting_mode == 'series'):
-        return read_nc_multi_series(sc, file_list, partitions) 
-    else:
-        raise NotImplementedError("You specified a mode that is not implemented.")
-
-def read_nc_single_time(sc, filename, partitions):
-    dset = xr.open_dataset(filename)
-
-    # Get all time steps
-    timesteps = dset.time
+    # create a list of dictionaries for  positional indexing
+    positional_indices = [dict(zip(partition_on, ij)) for ij in dim_cartesian_product_indices]
 
     if not partitions:
-        partitions = timesteps.size / 6
+        partitions = len(dim_cartesian_product_indices) / 50
 
-    if partitions > timesteps.size:
-        partitions = timesteps.size
+    if partitions > len(dim_cartesian_product_indices):
+        partitions = len(dim_cartesian_product_indices)
 
-    rdd = sc.parallelize(timesteps.values, partitions)\
-            .map(lambda x: readone_timestep(dset, x))
-
-    return rdd 
-
-
-
-
-def read_nc_single_grid(sc, filename, partitions):
-     
-    dset = xr.open_dataset(filename)
-
-    # Get latitude and longitude values
-    lats = dset.lat.values
-    lons = dset.lon.values
-    grid_points = [element for element in itertools.product(lats, lons)]
-
-    if not partitions:
-        partitions = len(grid_points) / 20 
-
-    if partitions > len(grid_points):
-        partitions = len(grid_points)
-
-    rdd = sc.parallelize(grid_points, partitions)\
-            .map(lambda x: readone_gridpoint(dset, x))
+    
+    # Create an RDD
+    rdd = sc.parallelize(positional_indices, partitions).map(lambda x: readone_slice(dset, x))
 
     return rdd
-
-
-def readone_gridpoint(dset, grid_point):
-    chunk = dset.sel(lat=grid_point[0], lon=grid_point[1])
-    return chunk
-
-def read_nc_multi_time_slice(sc, paths, partitions):
-    if isinstance(paths, list):
-        file_list = paths
-    elif isinstance(paths, str):
-        file_list = sorted(glob(paths))
-    
-    if not partitions:
-        partitions = len(file_list) / 20
-
-    if partitions > len(file_list):
-        partitions = len(file_list)
-
-    rdd = sc.parallelize(file_list, partitions)\
-           .map(lambda filename: readones(filename))
-
-    return rdd
-        
-def readones(filename):
-    dset = xr.open_dataset(filename)
-    return dset
-
-def read_nc_multi_series(sc, file_list, partitions):
-    pass
-
-
-'''
-if __name__ == '__main__':
-    spark = SparkSession.builder.appName('hi').getOrCreate()
-    sc = spark.sparkContext
-    
-    
-    print(os.getcwd())
-    filename = os.path.abspath('data/air.sig995.2012.nc')
-    print(filename)
-
-    #rdd = ncread(sc, filename, mode='single', partition_on='grid')
-    #print(rdd.count())
-    #print(rdd.first())
-    #print(rdd.getNumPartitions())
-    #print(('################'))
-    rdd1 = ncread(sc, filename, mode='single', partition_on='time')
-    print(rdd1.count())
-    print(rdd1.first())
-    print(rdd1.getNumPartitions())
-    
-    """
-    filepath = '/Users/abanihi/Documents/netCDF-datasets/NCEP-OI/*.nc'
-    #rdd = ncread(sc, filepath, mode='multi', partition_on='time')
-    rdd = nc_multi_read(sc, filepath, data_splitting_mode='slice')
-    print(rdd.count())
-    print(type(rdd.first()))
-    """
-'''
-
 
--- sparkxarray/tests/test_reader.py
+++ sparkxarray/tests/test_reader.py
@@ -3,12 +3,15 @@ from sparkxarray.reader import ncread
 import os
 
 
-spark = SparkSession.builder.appName('hi').getOrCreate()
+spark = SparkSession.builder.appName('spark-tests').getOrCreate()
 sc = spark.sparkContext
 print(os.getcwd())
 filename = os.path.abspath('sparkxarray/tests/data/air.sig995.2012.nc')
 print(filename)
+paths = os.path.abspath('sparkxarray/tests/data/NCEP/*.nc')
+print(paths)
 
+### Tests for single file
 rdd1 = ncread(sc, filename, mode='single', partition_on=['lat', 'lon'], partitions=300)
 print(rdd1.count())
 print(rdd1.first())
@@ -20,9 +23,17 @@ print(rdd2.count())
 print(rdd2.first())
 
 
-rdd3 = ncread(sc, filename, mode='single', partition_on=['time', 'lat', 'lon'], partitions=50000)
+### Tests for Multiple files 
+rdd3 = ncread(sc, paths, mode='multi', partition_on=['lat', 'lon'], partitions=300)
 print(rdd3.count())
 print(rdd3.first())
+
+rdd4 = ncread(sc, paths, mode='multi', partition_on=['lat', 'lon', 'time', 'nv'], partitions=1000)
+print(rdd4.count())
+print(rdd4.first())
+
+
+
 
 
 
