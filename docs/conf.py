8f616dec91acc1c4c848652308c8f519cdf105d2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-08-14 20:23:32 -0500

    Add gitignore and update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,18 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+# Get the project root dir, which is the parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+
+# Insert the project root dir as the first element in the PYTHONPATH.
+# This lets us ensure that the source package is imported, and that its
+# version is used.
+sys.path.insert(0, project_root)
+
+import sparkxarray
 # sys.path.insert(0, os.path.abspath('.'))
 
 
