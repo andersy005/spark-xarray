f19c4948b97bfa748aea8fd63155fee4c34f8275
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-10-27 10:22:14 -0500

    Add utilities module

---
+++ sparkxarray/utils/vis_utils.py
@@ -0,0 +1,7 @@
+"""Utilities related to model visualization."""
+#import holoviews as hv
+#import geoviews as gv
+#import geoviews.feature as gf
+#from cartopy import crs
+
+#hv.notebook_extension()
